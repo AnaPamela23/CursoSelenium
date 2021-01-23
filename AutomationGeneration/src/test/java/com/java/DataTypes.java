835867956b4b619ef4433ff2006c4ab6328c61ba
AnaPamela23 <pamela23_zc@hotmail.com>
2021-01-23 12:26:50 -0600

    Tipos de Datos

---
+++ AutomationGeneration/src/test/java/com/java/DataTypes.java
@@ -0,0 +1,23 @@
+package com.java;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		byte variableByte = 127;
+		int variableInt = 1234;
+		System.out.println(variableInt);
+		System.out.println();
+
+		double altura = 2.54;
+		System.out.println("Esta es la altura " + altura);
+		
+		char letra1 = '\u0048';
+		letra1 = '\u0039';
+		System.out.println(letra1);
+				
+				
+	}
+
+}

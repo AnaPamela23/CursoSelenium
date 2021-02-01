e709abff6af0dfb3870b9b322b2aefc939955855
AnaPamela23 <pamela23_zc@hotmail.com>
2021-01-31 22:49:51 -0600

    Operadores 2

---
+++ AutomationGeneration/src/test/java/com/java/Operators.java
@@ -0,0 +1,69 @@
+package com.java;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		
+		//Aritmeticos
+		
+		int x= 2;
+		int y= 3; 
+		
+		int z= x * y;
+		System.out.println("Este es el valor de z:" + z);
+		
+//		Unarios
+		
+		int A= 1;
+		int B= 2;
+		
+//		System.out.println(-A);
+//		System.out.println(--A);
+//		System.out.println(-B);
+//		System.out.println(--B);
+//		System.out.println(+A);
+//		System.out.println(++A);
+//		System.out.println(+B);
+//		System.out.println(++B);
+		
+//		Relacionales
+		
+//		if (A == B) {
+			System.out.println("A y B son valores diferentes");
+			
+//			if (A != B) {
+				System.out.println("A y B son valores diferentes");
+				
+//			if (A > B) {
+				System.out.println("A y B son valores diferentes");
+					
+//			if (A >= B) {
+				System.out.println("A y B son valores diferentes");
+						
+//			if (A < B) {
+				System.out.println("A y B son valores diferentes");
+							
+//			if (A <= B) {
+				System.out.println("A y B son valores diferentes");
+		
+				
+//				Condicionales
+				
+				int var1 =5;
+				int var2 =5;
+				int var3 =3;
+				int var4 =6;
+				if (var1 == var2 && var3 == var4) {		
+					System.out.println("Var 1 y 2 iguales y var3 y var4 iguales");
+				}
+				
+				if (var1 == var2 || var3 == var4) {		
+					System.out.println("var1 y var2 son iguales o var3 y var4 son iguales");
+				}
+		}
+		
+		
+
+	}
+
+

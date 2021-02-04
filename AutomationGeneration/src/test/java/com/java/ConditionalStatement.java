07e102499fbff4673e73ea2cdd003594e8752106
AnaPamela23 <pamela23_zc@hotmail.com>
2021-02-04 17:02:48 -0600

    Bucles

--- AutomationGeneration/src/test/java/com/java/ConditionalStatement.java
+++ AutomationGeneration/src/test/java/com/java/ConditionalStatement.java
@@ -33,6 +33,29 @@ public class ConditionalStatement {
 	}else {
 		System.out.println("El coche esta en una calle y va a una velocidad adecuada");
 	}
-
+//	SWITCH CASE
+	
+	int temperatura = 5;
+	switch (temperatura) {
+	
+	case 5:
+		System.out.println("Mucho frio");
+		break;
+	
+	case 10:
+		System.out.println("frio");
+		break;
+		
+	case 20:
+		System.out.println("Clima agradable");
+		break;
+		
+	case 30:
+		System.out.println("Calor");
+		break;
+	
+	default:
+		System.out.println("No encontro relacion con la temperatura");
 	}
+}
 }

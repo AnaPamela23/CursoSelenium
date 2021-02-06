cfaebc4ff23757fe6f2493b623b4ee8dd3b65e3b
AnaPamela23 <pamela23_zc@hotmail.com>
2021-02-06 11:59:00 -0600

    Herencias y polimorfismos

---
+++ AutomationGeneration/src/test/java/com/excersise/TcketsParks.java
@@ -0,0 +1,5 @@
+package com.excersise;
+
+public abstract class TcketsParks {
+
+}
---
+++ AutomationGeneration/src/test/java/com/excersise/TicketDesk.java
@@ -0,0 +1,10 @@
+package com.excersise;
+
+public class TicketDesk {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

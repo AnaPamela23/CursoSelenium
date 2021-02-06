130f0457784d3e911e3ba61ab1a6d021b281a2a5
AnaPamela23 <pamela23_zc@hotmail.com>
2021-02-06 11:59:19 -0600

    Herencias y polimorfimo

--- AutomationGeneration/src/test/java/com/excersise/TcketsParks.java
+++ AutomationGeneration/src/test/java/com/excersise/TcketsParks.java
@@ -1,5 +1,17 @@
 package com.excersise;
 
 public abstract class TcketsParks {
+	String welcomMessage;
+	public TcketsParks() {
+		this.welcomMessage = "Welcome Cooltesters Park ";
+	}
+public String welcomeMessageString() {
+	return this.welcomMessage;
+}
+
+//Abstraction
+public abstract double getTicketPrice();
+public abstract void printPriceDay();
+public abstract void printPriceDay();(String keyManager, String priceDay);
 
 }
--- AutomationGeneration/src/test/java/com/excersise/TicketDesk.java
+++ AutomationGeneration/src/test/java/com/excersise/TicketDesk.java
@@ -1,10 +1,16 @@
 package com.excersise;
 
-public class TicketDesk {
+public class TicketDesk{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public static void main(String[] args) {
 
-	}
-
+	double precioCoolPark=150;
+	double precioTestersPark=160;
+	int age=25;
+	boolean student=true;
+		
+		CoolParkTicket coolPark = new CoolParkTicket(100,10,true);
+		coolPark.printPriceDay("xyz", "Dia del niño");
+		coolPark.getTicketPrice();
+		}
 }
